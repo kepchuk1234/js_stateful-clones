b70f788bfba5fde439e44625601838280812c497
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-15 23:51:34 +0200

    develop

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,46 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let modyState = { ...state };
+
+  function removeProp(obj, properties) {
+    for (const property of properties) {
+      delete obj[property];
+    }
+  }
+
+  function clear(obj) {
+    for (const key of Object.keys(obj)) {
+      delete obj[key];
+    }
+  }
+
+  function addProp(obj, properties) {
+    Object.assign(obj, properties);
+  }
+
+  for (const action of actions) {
+    const newObj = { ...modyState };
+
+    switch (action.type) {
+      case 'addProperties':
+        addProp(newObj, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProp(newObj, action.keysToRemove);
+        break;
+
+      case 'clear':
+        clear(newObj);
+    }
+
+    result.push(newObj);
+    modyState = newObj;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
